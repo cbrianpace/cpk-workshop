c9a75abbd16644a9830e890d04d465fd3337b7d3
bpace <31553606+cbrianpace@users.noreply.github.com>
2024-07-22 23:57:09 -0400

    fix init sql and command txt

--- hippo/init.sql
+++ hippo/init.sql
@@ -1,5 +1,3 @@
-alter user postgres with login password 'welcome1';
-
 create table employee (empid int, first_name varchar(40), last_name varchar(40), status varchar(10), hiredt date, terminationdt date);
 alter table employee add primary key (empid);
 create table department (deptid int, department varchar(40), glcode varchar(20));
